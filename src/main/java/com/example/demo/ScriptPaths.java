077c250327684186444ec6d63dce295114718eb9
Amol Nakhate <amol64546@gmail.com>
2024-04-17 09:50:23 +0530

    Refactor ScriptPaths class and move script files
    The BASE_PATH has been introduced in the ScriptPaths class to reduce redundancy in defining the paths. The scripts have been moved from the resources directory to the scripts directory under main/java. Consequently, the paths to these files have been updated to incorporate these changes.

--- src/main/java/com/example/demo/ScriptPaths.java
+++ src/main/java/com/example/demo/ScriptPaths.java
@@ -3,13 +3,15 @@ package com.example.demo;
 public class ScriptPaths
 {
 
-    public final static String TEST = "/home/amol/Downloads/demo/src/main/resources/test.sh";
+    private final static String BASE_PATH = "/home/amol/Downloads/demo/src/main/java/com/example/demo/scripts/";
 
-    public final static String INSTALL_MONGO = "/home/amol/Downloads/demo/src/main/resources/install/mongo.sh";
-    public final static String INSTALL_POSTGRES = "/home/amol/Downloads/demo/src/main/resources/install/postgres.sh";
+    public final static String TEST = BASE_PATH+ "test.sh";
+
+    public final static String INSTALL_MONGO = BASE_PATH + "install/mongo.sh";
+    public final static String INSTALL_POSTGRES = BASE_PATH + "install/postgres.sh";
 
 
-    public final static String UNINSTALL_MONGO = "/home/amol/Downloads/demo/src/main/resources/uninstall/mongo.sh";
-    public final static String UNINSTALL_POSTGRES = "/home/amol/Downloads/demo/src/main/resources/uninstall/postgres.sh";
+    public final static String UNINSTALL_MONGO = BASE_PATH + "uninstall/mongo.sh";
+    public final static String UNINSTALL_POSTGRES = BASE_PATH + "uninstall/postgres.sh";
 }
 
